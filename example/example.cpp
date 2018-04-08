98abff7010cbc866062aa3a77ad10a0a36bd8cdb
savagecm <savagecm@qq.com>
2018-04-08 14:04:46 +0800

    first

--- example/example.cpp
+++ example/example.cpp
@@ -1,20 +1,14 @@
 
 #include "logger/logger.hpp"
-
+#include "mpmc/mpmc.hpp"
 int main()
 {
     set_log_level(logger_iface::log_level::warn);
-    set_max_log_buff(10);
-    for (int i = 0; i < 100; i++)
-    {
-        __LOG(error, "hello logger!"
-                         << "this is error log");
-        __LOG(warn, "hello logger!"
-                        << "this is warn log");
-        __LOG(info, "hello logger!"
-                        << "this is info log");
-        __LOG(debug, "hello logger!"
-                         << "this is debug log");
-    }
-    dump_log();
+
+    auto tmp = new MPMCQueue<void *>(100);
+    tmp->enqueue((void *)1);
+    void *element = NULL;
+    tmp->dequeue(element);
+
+    __LOG(error, "enement is : " << (void *)element);
 }
